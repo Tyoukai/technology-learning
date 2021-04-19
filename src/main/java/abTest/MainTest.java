e4052b15daa2a24724b0a86ba88308c11ec30cf1
zhangkewei <zhang_k_w@163.com>
2021-04-19 12:58:52 +0800

    完成abTest的基本原型

--- src/main/java/abTest/MainTest.java
+++ src/main/java/abTest/MainTest.java
@@ -1,9 +1,5 @@
 package abTest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -13,9 +9,8 @@ import org.apache.commons.lang3.StringUtils;
  * Created on 2021-04-16
  */
 public class MainTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         OuterApi api = new OuterApi();
-        List<String> list = new ArrayList<>();
 
         AbTestHelper<Person, PersonNew> helper = new AbTestHelper<>((person, person1) -> {
             if (person == null || person1 == null) {
@@ -26,10 +21,22 @@ public class MainTest {
                 return 0;
             }
             return -1;
-        }, null, null);
+        }, (aResult, bResult) -> {
+            System.out.println(aResult);
+            System.out.println(bResult);
+        }, personNew -> {
+            Person person = new Person();
+            person.setIdCard(personNew.getIdCard());
+            person.setAge(personNew.getAge());
+            person.setName(personNew.getName());
+            return person;
+        });
         String idCard = "123";
         while (true) {
             Person result = helper.doABTest(() -> api.api1(idCard), () -> api.api2(idCard), Long.parseLong(idCard));
+            System.out.println(result);
+            System.out.println("==========");
+            Thread.sleep(1000);
         }
 
     }
