370f196b8f2c567ba274be53c85f0db45a0f0726
Tyoukai <zhang_k_w@163.com>
2021-11-21 12:09:27 +0800

    rename

--- src/main/java/guava/JoinerSplitterTest.java
+++ src/main/java/guava/JoinerSplitterTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class StringUtilsTest {
+public class JoinerSplitterTest {
     public static void main(String[] args) {
         join();
         split();
