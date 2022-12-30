94b29ca446b220995b287db410fa247f4a476411
Tyoukai <zhang_k_w@163.com>
2022-12-30 13:15:23 +0800

    增加了TDengine demo

--- src/main/java/dom4j/Dom4jDemo.java
+++ src/main/java/dom4j/Dom4jDemo.java
@@ -1,9 +1,6 @@
 package dom4j;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
+import org.dom4j.*;
 
 import java.util.List;
 
@@ -13,139 +10,313 @@ import java.util.List;
  */
 public class Dom4jDemo {
     private static  String XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<java version=\"1.8.0_265\" class=\"java.beans.XMLDecoder\">\n" +
+            "<java version=\"1.7.0_79\" class=\"java.beans.XMLDecoder\">\n" +
             " <object class=\"java.util.HashMap\">\n" +
             "  <void method=\"put\">\n" +
-            "   <string>fd_3a0fc15e03fab2</string>\n" +
-            "   <object class=\"java.util.HashMap\">\n" +
-            "    <void method=\"put\">\n" +
-            "     <string>name</string>\n" +
-            "     <string>鲁雄锋</string>\n" +
-            "    </void>\n" +
-            "    <void method=\"put\">\n" +
-            "     <string>id</string>\n" +
-            "     <string>15aecaa496471ef76e4aeaf44569a357</string>\n" +
-            "    </void>\n" +
-            "   </object>\n" +
+            "   <string>fd_3a5bfaa8549c3a</string>\n" +
+            "   <string>张珂为-技术组成员</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>attrCiName</string>\n" +
-            "   <string>测试22</string>\n" +
+            "   <string>fd_3a917fc8207a28</string>\n" +
+            "   <string>0</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>fd_3a030486eb9d7e</string>\n" +
-            "   <string>自研发</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_38_</string>\n" +
-            "   <string>测试22</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_165_</string>\n" +
-            "   <string></string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_115_</string>\n" +
+            "   <string>fd_3a917ffda861ec</string>\n" +
             "   <string>1</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_48_</string>\n" +
-            "   <string>chencp1</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>attrCiId</string>\n" +
-            "   <string>测试22</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>network</string>\n" +
-            "   <string>测试22</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_34_</string>\n" +
-            "   <null/>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_33_</string>\n" +
-            "   <string>否</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_22__text</string>\n" +
-            "   <string>集中交易</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_51_</string>\n" +
-            "   <string>luxf1</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_71_</string>\n" +
-            "   <string>本地备份</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>col_5_</string>\n" +
-            "   <string>二类</string>\n" +
-            "  </void>\n" +
-            "  <void method=\"put\">\n" +
-            "   <string>fd_3a0fbe23f10ff4</string>\n" +
-            "   <object class=\"java.util.HashMap\">\n" +
-            "    <void method=\"put\">\n" +
-            "     <string>name</string>\n" +
-            "     <string>陈传鹏</string>\n" +
+            "   <string>fd_3a5be7cf8e288a</string>\n" +
+            "   <object class=\"java.util.ArrayList\">\n" +
+            "    <void method=\"add\">\n" +
+            "     <object class=\"java.util.HashMap\">\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fd_3a5bfbf9c855ce</string>\n" +
+            "       <object class=\"java.util.HashMap\">\n" +
+            "        <void method=\"put\">\n" +
+            "         <string>id</string>\n" +
+            "         <string>141112c05e9be33cb1793fb4b95846b0</string>\n" +
+            "        </void>\n" +
+            "        <void method=\"put\">\n" +
+            "         <string>name</string>\n" +
+            "         <string>长江保荐</string>\n" +
+            "        </void>\n" +
+            "       </object>\n" +
+            "      </void>\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fdId</string>\n" +
+            "       <string>18519e5f65c6d42304ec0f5456bb5d14</string>\n" +
+            "      </void>\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fd_3a5beac5380b36</string>\n" +
+            "       <string>备注1</string>\n" +
+            "      </void>\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fd_3a5bea90ecc37a</string>\n" +
+            "       <double>50.0</double>\n" +
+            "      </void>\n" +
+            "     </object>\n" +
             "    </void>\n" +
-            "    <void method=\"put\">\n" +
-            "     <string>id</string>\n" +
-            "     <string>1541acbd8ab8a8206dbb16046b2b3e82</string>\n" +
+            "    <void method=\"add\">\n" +
+            "     <object class=\"java.util.HashMap\">\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fd_3a5bfbf9c855ce</string>\n" +
+            "       <object class=\"java.util.HashMap\">\n" +
+            "        <void method=\"put\">\n" +
+            "         <string>id</string>\n" +
+            "         <string>1411138cf2f89c6fa3427db4da783ee7</string>\n" +
+            "        </void>\n" +
+            "        <void method=\"put\">\n" +
+            "         <string>name</string>\n" +
+            "         <string>长江证券行政序列_中后台部门_法律合规部</string>\n" +
+            "        </void>\n" +
+            "       </object>\n" +
+            "      </void>\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fdId</string>\n" +
+            "       <string>18519e5f65cffa2eeafd245445fae604</string>\n" +
+            "      </void>\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fd_3a5beac5380b36</string>\n" +
+            "       <string>备注2</string>\n" +
+            "      </void>\n" +
+            "      <void method=\"put\">\n" +
+            "       <string>fd_3a5bea90ecc37a</string>\n" +
+            "       <double>50.0</double>\n" +
+            "      </void>\n" +
+            "     </object>\n" +
             "    </void>\n" +
             "   </object>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_74__text</string>\n" +
-            "   <string>经纪业务集中交易系统</string>\n" +
+            "   <string>fd_3a917fa74d35d8</string>\n" +
+            "   <string>项目筛选表</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_27_</string>\n" +
-            "   <string>P2</string>\n" +
+            "   <string>fd_3a5be502e328bc</string>\n" +
+            "   <string>立项原因说明</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>fd_39ff2846831c6a</string>\n" +
-            "   <string>success</string>\n" +
+            "   <string>fd_3a5bf8d7db18d2</string>\n" +
+            "   <string>1、完成阶段目标1&#13;\n" +
+            "2、完成阶段目标2</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_37_</string>\n" +
-            "   <string>否</string>\n" +
+            "   <string>fd_3a5be33bc763b4</string>\n" +
+            "   <object class=\"java.util.HashMap\">\n" +
+            "    <void method=\"put\">\n" +
+            "     <string>id</string>\n" +
+            "     <string>141116326e34bd3e3fdd44646b5a06cd</string>\n" +
+            "    </void>\n" +
+            "    <void method=\"put\">\n" +
+            "     <string>name</string>\n" +
+            "     <string>长江期货_公司领导</string>\n" +
+            "    </void>\n" +
+            "   </object>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_45_</string>\n" +
-            "   <string>A 客户服务类</string>\n" +
+            "   <string>fd_3a5bfa2a050850</string>\n" +
+            "   <string>张珂为-项目经理</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_89_</string>\n" +
+            "   <string>fd_3a7484e4ad28c2</string>\n" +
             "   <string>2</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_22_</string>\n" +
-            "   <string>集中交易</string>\n" +
+            "   <string>fd_3a5be19e11779c</string>\n" +
+            "   <string>项目立项申请表</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_74_</string>\n" +
-            "   <string>经纪业务集中交易系统</string>\n" +
+            "   <string>fd_3a917fffc3cfe6</string>\n" +
+            "   <string>0</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>fd_3a030486eb9d7e_text</string>\n" +
-            "   <string>自研发</string>\n" +
+            "   <string>fd_3a5bee9adcd874</string>\n" +
+            "   <string>table_1_start</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_50_</string>\n" +
-            "   <string>敏态</string>\n" +
+            "   <string>fd_3a75eef5b45150</string>\n" +
+            "   <string>table_1_end</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>col_72_</string>\n" +
-            "   <string>三级</string>\n" +
+            "   <string>fd_3a917fb88dd9a4</string>\n" +
+            "   <string>0</string>\n" +
             "  </void>\n" +
             "  <void method=\"put\">\n" +
-            "   <string>fd_39ff0eb5def15a</string>\n" +
-            "   <object class=\"java.util.Date\">\n" +
-            "    <long>1670256000000</long>\n" +
+            "   <string>fd_3a917ffd503c9e</string>\n" +
+            "   <string>1</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bf7946aa642</string>\n" +
+            "   <string>时间</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bf6ed941708</string>\n" +
+            "   <object class=\"java.math.BigDecimal\">\n" +
+            "    <string>14475</string>\n" +
             "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a917ffeca7af2</string>\n" +
+            "   <string>1</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bf81b069702</string>\n" +
+            "   <object class=\"java.util.Date\">\n" +
+            "    <long>1671120000000</long>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a917ffe753544</string>\n" +
+            "   <string>0</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5be2c2f91624</string>\n" +
+            "   <string>1</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bfa72c1fffe</string>\n" +
+            "   <string>张珂为-业务代表</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_4_2</string>\n" +
+            "   <object class=\"java.math.BigDecimal\">\n" +
+            "    <string>111</string>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_4_3</string>\n" +
+            "   <string>备注5</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_4_1</string>\n" +
+            "   <string>资本性支出-长期待摊费用</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a917fff4978e4</string>\n" +
+            "   <string>1</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5be6dc149664</string>\n" +
+            "   <string>项目成本分摊方式</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5be7cf8e288a_index_</string>\n" +
+            "   <object class=\"java.util.HashMap\">\n" +
+            "    <void method=\"put\">\n" +
+            "     <string>fd_3a5bfbf9c855ce</string>\n" +
+            "     <null/>\n" +
+            "    </void>\n" +
+            "    <void method=\"put\">\n" +
+            "     <string>fd_3a5beac5380b36</string>\n" +
+            "     <null/>\n" +
+            "    </void>\n" +
+            "    <void method=\"put\">\n" +
+            "     <string>fd_3a5bea90ecc37a</string>\n" +
+            "     <null/>\n" +
+            "    </void>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bfa9a054ba6</string>\n" +
+            "   <string>张珂为-技术代表</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bf89d63cf74</string>\n" +
+            "   <object class=\"java.util.Date\">\n" +
+            "    <long>1672329600000</long>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_3_3</string>\n" +
+            "   <string>备注3</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_3_1</string>\n" +
+            "   <string>资本性支出-硬件</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bf9c7dd57f8</string>\n" +
+            "   <string>人员</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_3_2</string>\n" +
+            "   <object class=\"java.math.BigDecimal\">\n" +
+            "    <string>22</string>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_2_1</string>\n" +
+            "   <string>资本性支出-软件</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_1_3</string>\n" +
+            "   <string>明细</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_1_1</string>\n" +
+            "   <string>类别</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_1_2</string>\n" +
+            "   <string>金额（万元）</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_2_2</string>\n" +
+            "   <object class=\"\t\">\n" +
+            "    <string>100</string>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_2_3</string>\n" +
+            "   <string>备注4</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bfa5d96c1ea</string>\n" +
+            "   <string>张珂为-业务组成员</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_5_2</string>\n" +
+            "   <object class=\"java.math.BigDecimal\">\n" +
+            "    <string>2121</string>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_5_1</string>\n" +
+            "   <string>费用性支出-技术开发费用</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5bee6c0a58c4</string>\n" +
+            "   <string>项目资源</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_5_3</string>\n" +
+            "   <string>备注6</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a7674e35f4d6a</string>\n" +
+            "   <string>1、因财务总部对项目预算资本性支出与费用化支出严格分类管控，请在提交预算金额时尽量预估准确。/n2、长期分摊费用：折旧年限在1年以上（不含1年）的各项费用。如一次性支付1年以上的费用。/n3、费用性支出：折旧年限在一年以下（含1年）的各项费用。</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_6_3</string>\n" +
+            "   <string>备注7</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_6_2</string>\n" +
+            "   <object class=\"java.math.BigDecimal\">\n" +
+            "    <string>12121</string>\n" +
+            "   </object>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5be25815d65c</string>\n" +
+            "   <string>原始项目立项测试</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3a5be5d6d4c492</string>\n" +
+            "   <string>立项目标说明</string>\n" +
+            "  </void>\n" +
+            "  <void method=\"put\">\n" +
+            "   <string>fd_3_6_1</string>\n" +
+            "   <string>费用性支出-其他费用</string>\n" +
             "  </void>\n" +
             " </object>\n" +
             "</java>\n";
@@ -162,6 +333,25 @@ public class Dom4jDemo {
 
         Element objectElement = javaElement.element("object");
         List<Element> voidElementList = objectElement.elements("void");
+
+        for (Element element : voidElementList) {
+//            System.out.println(element.attribute("method").getData());
+            List<Element> strElements = element.elements("string");
+            if (strElements.size() == 2) {
+                System.out.println("===========");
+                System.out.println(strElements.get(0).getTextTrim());
+                System.out.println(strElements.get(1).getTextTrim());
+            }
+
+            List<Element> objElements = element.elements("object");
+            if (objElements.size() == 1) {
+                Attribute attribute = objElements.get(0).attribute("class");
+                if (attribute.getData().equals("java.math.BigDecimal")) {
+                    System.out.println("数值类型：" + objElements.get(0).element("string").getData() );
+                }
+            }
+        }
+
 
 
 
---
+++ src/main/java/tDengine/TDengineDemo.java
@@ -0,0 +1,101 @@
+package tDengine;
+
+import com.taosdata.jdbc.TSDBDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Echo
+ * @Date 2022/12/29 9:11
+ */
+//public class TDengineDemo {
+//    public static void main(String[] args) throws SQLException {
+//        String jdbcUrl = "jdbc:TAOS://172.16.66.14:6030/test_zkw";
+//        Properties connProps = new Properties();
+//        connProps.setProperty(TSDBDriver.PROPERTY_KEY_CHARSET, "UTF-8");
+//        connProps.setProperty(TSDBDriver.PROPERTY_KEY_LOCALE, "en_US.UTF-8");
+//        connProps.setProperty(TSDBDriver.PROPERTY_KEY_TIME_ZONE, "UTC-8");
+//        Connection conn = DriverManager.getConnection(jdbcUrl, connProps);
+//        System.out.println("Connected");
+//        conn.close();
+//    }
+//}
+
+
+
+public class TDengineDemo {
+    private static Connection getConnection() throws SQLException {
+        String jdbcUrl = "jdbc:TAOS://172.16.66.14:6030?user=root&password=taosdata";
+        System.out.println("path" + System.getProperty("java.library.path"));
+
+//        String jdbcUrl = "jdbc:TAOS-RS://172.16.66.18:6041?user=root&password=taosdata";
+        return DriverManager.getConnection(jdbcUrl);
+    }
+
+    private static List<String> getRawData() {
+        return Arrays.asList(
+                "d1001,2018-10-03 14:38:05.000,10.30000,219,0.31000,'California.SanFrancisco',2",
+                "d1001,2018-10-03 14:38:15.000,12.60000,218,0.33000,'California.SanFrancisco',2",
+                "d1001,2018-10-03 14:38:16.800,12.30000,221,0.31000,'California.SanFrancisco',2",
+                "d1002,2018-10-03 14:38:16.650,10.30000,218,0.25000,'California.SanFrancisco',3",
+                "d1003,2018-10-03 14:38:05.500,11.80000,221,0.28000,'California.LosAngeles',2",
+                "d1003,2018-10-03 14:38:16.600,13.40000,223,0.29000,'California.LosAngeles',2",
+                "d1004,2018-10-03 14:38:05.000,10.80000,223,0.29000,'California.LosAngeles',3",
+                "d1004,2018-10-03 14:38:06.500,11.50000,221,0.35000,'California.LosAngeles',3"
+        );
+    }
+
+
+    /**
+     * The generated SQL is:
+     * INSERT INTO power.d1001 USING power.meters TAGS(California.SanFrancisco, 2) VALUES('2018-10-03 14:38:05.000',10.30000,219,0.31000)
+     * power.d1001 USING power.meters TAGS(California.SanFrancisco, 2) VALUES('2018-10-03 14:38:15.000',12.60000,218,0.33000)
+     * power.d1001 USING power.meters TAGS(California.SanFrancisco, 2) VALUES('2018-10-03 14:38:16.800',12.30000,221,0.31000)
+     * power.d1002 USING power.meters TAGS(California.SanFrancisco, 3) VALUES('2018-10-03 14:38:16.650',10.30000,218,0.25000)
+     * power.d1003 USING power.meters TAGS(California.LosAngeles, 2) VALUES('2018-10-03 14:38:05.500',11.80000,221,0.28000)
+     * power.d1003 USING power.meters TAGS(California.LosAngeles, 2) VALUES('2018-10-03 14:38:16.600',13.40000,223,0.29000)
+     * power.d1004 USING power.meters TAGS(California.LosAngeles, 3) VALUES('2018-10-03 14:38:05.000',10.80000,223,0.29000)
+     * power.d1004 USING power.meters TAGS(California.LosAngeles, 3) VALUES('2018-10-03 14:38:06.500',11.50000,221,0.35000)
+     */
+    private static String getSQL() {
+        StringBuilder sb = new StringBuilder("INSERT INTO ");
+        for (String line : getRawData()) {
+            String[] ps = line.split(",");
+            sb.append("power." + ps[0]).append(" USING power.meters TAGS(")
+                    .append(ps[5]).append(", ") // tag: location
+                    .append(ps[6]) // tag: groupId
+                    .append(") VALUES(")
+                    .append('\'').append(ps[1]).append('\'').append(",") // ts
+                    .append(ps[2]).append(",") // current
+                    .append(ps[3]).append(",") // voltage
+                    .append(ps[4]).append(") "); // phase
+        }
+        return sb.toString();
+    }
+
+    public static void insertData() throws SQLException {
+        try (Connection conn = getConnection()) {
+            try (Statement stmt = conn.createStatement()) {
+//                stmt.execute("CREATE DATABASE power KEEP 3650");
+//                stmt.execute("CREATE STABLE power.meters (ts TIMESTAMP, current FLOAT, voltage INT, phase FLOAT) " +
+//                        "TAGS (location BINARY(64), groupId INT)");
+                String sql = getSQL();
+                int rowCount = stmt.executeUpdate(sql);
+                System.out.println("rowCount=" + rowCount); // rowCount=8
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        System.load("C:\\TDengine\\driver\\taos.dll");
+        insertData();
+    }
+}
